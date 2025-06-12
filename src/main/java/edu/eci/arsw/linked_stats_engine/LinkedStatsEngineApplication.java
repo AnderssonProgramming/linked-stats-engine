7ec18b33a82329256a1b803a866dd0fdc87859a3
AnderProgramming <158221956+AnderssonProgramming@users.noreply.github.com>
2025-06-12 12:29:11 -0500

    feat: add linked-stats-engine project scaffolding, generated with spring initialzr

---
+++ src/main/java/edu/eci/arsw/linked_stats_engine/LinkedStatsEngineApplication.java
@@ -0,0 +1,13 @@
+package edu.eci.arsw.linked_stats_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LinkedStatsEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LinkedStatsEngineApplication.class, args);
+	}
+
+}
